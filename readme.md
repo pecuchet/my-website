8b76dca138345596e916e364cc39575ae32c166f
pecuchet <code@dotburo.org>
2021-01-09 21:53:39 +0100

    Added license info

--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@
 The code is licensed under a [General Public License (GPL-3.0)](LICENSE).
 
 #### Content licence
-The content is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](content/LICENSE).
+The content is licensed under a Creative Commons [Attribution-NonCommercial-ShareAlike 4.0 International License](content/LICENSE) (CC BY-NC-SA 4.0).
--- themes/db-5/js/pageTpl.js
+++ themes/db-5/js/pageTpl.js
@@ -42,7 +42,8 @@ define([], function () {
                 }
                 li.push( liString );
             }
-            return html + '<ul>' + li.join('') + '</ul>';
+            return html + '<ul>' + li.join('') + '</ul>'
+                + '<footer><p><small>This work is licensed under a Creative Commons <a rel="license" itemprop="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">Attribution-NonCommercial-ShareAlike 4.0 International License </a>(CC BY-NC-SA 4.0).</small><br><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"></a></p></footer>';
         }
     }
 });
